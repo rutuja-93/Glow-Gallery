b9e89092e48924a8d88f6c994d3371bf052d85bd
rutuja-93 <rutujazaware87@gmail.com>
2025-07-17 17:15:52 +0530

    photo galary

---
+++ script.js
@@ -0,0 +1,15 @@
+const images = document.querySelectorAll(".gallery img");
+const lightbox = document.querySelector(".lightbox");
+const lightboxImg = document.querySelector(".lightbox-img");
+const closeBtn = document.querySelector(".close");
+
+images.forEach(img => {
+  img.addEventListener("click", () => {
+    lightbox.style.display = "flex";
+    lightboxImg.src = img.src;
+  });
+});
+
+closeBtn.addEventListener("click", () => {
+  lightbox.style.display = "none";
+});
